77f4c149bdd0edb9e5098e51853bce4c9b4f1200
nazorm <nnadoziechinazor@gmail.com>
2023-06-20 13:14:17 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int count = 0;
 	char letter;
 
-	for (i = 0; i < 10; i++)
+	while (count++ <= 9)
 	{
-		letter = 'a';
-		while (letter <= 'z')
-		{
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-			letter++;
-		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
