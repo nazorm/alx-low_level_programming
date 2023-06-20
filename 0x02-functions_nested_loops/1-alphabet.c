d372d90df71be9b7118941030f3226c5b16559ab
nazorm <nnadoziechinazor@gmail.com>
2023-06-20 12:10:51 -0700

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+    return (0);
+}
