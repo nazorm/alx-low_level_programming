3613bb657c3c3acf28f391794c36678c6f2afb80
nazorm <nnadoziechinazor@gmail.com>
2023-06-20 13:56:10 -0700

    betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,15 @@
 
 /**
  * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
  * Return: 1 if c is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return 1;
-	else
-		return 0;
+	if (c >= 'a' && c <= 'z'){
+            return 1;
+	}
+	else{
+	return 0;
+	}
 }
